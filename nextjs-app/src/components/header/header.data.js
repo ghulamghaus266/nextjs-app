ae8c24903869061f5da039d8ff124b4e710f0bcf
Ghulam Ghaus <74096351+ghulamghaus266@users.noreply.github.com>
2022-06-27 00:25:07 +0500

    add

---
+++ nextjs-app/src/components/header/header.data.js
@@ -0,0 +1,19 @@
+
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Feature',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonial',
+  },
+];
